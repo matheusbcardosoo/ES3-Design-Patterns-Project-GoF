53f675b15a1bf0674e9f3c868b7d7d5539783e50
Matheus Barbosa <96997944+matheusbcardosoo@users.noreply.github.com>
2025-06-27 21:19:46 -0300

    Feat: Create domain classes AND Application

---
+++ src/main/java/org/fatec/ClinicaVeterinariaApplication.java
@@ -0,0 +1,11 @@
+package org.fatec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaVeterinariaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicaVeterinariaApplication.class, args);
+    }
+}
